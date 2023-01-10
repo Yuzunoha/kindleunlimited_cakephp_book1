0d3f3afc2cfcfab45d6dbded5234c29ec3e7879e
Yuzunoha <studysteadystandby@gmail.com>
2023-01-11 00:31:49 +0900

    template

---
+++ html/messagepro/templates/Messages/add.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Message $message
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Messages'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="messages form content">
+            <?= $this->Form->create($message) ?>
+            <fieldset>
+                <legend><?= __('Add Message') ?></legend>
+                <?php
+                    echo $this->Form->control('user_id');
+                    echo $this->Form->control('category_id');
+                    echo $this->Form->control('status');
+                    echo $this->Form->control('title');
+                    echo $this->Form->control('body');
+                    echo $this->Form->control('create_datetime', ['empty' => true]);
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
